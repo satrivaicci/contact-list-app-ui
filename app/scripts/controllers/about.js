b2241d30b337e36aa529ba06db69a21a87adbe8e
angel espinoza <angel.espinoza@thermofisher.com>
2016-06-21 21:36:12 -0700

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name contactListUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the contactListUiApp
+ */
+angular.module('contactListUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
